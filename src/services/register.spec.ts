848bb108be68f5638b3a912c1d6f8f0ba12605e3
Eduardo Horstmann <eduardohorstmann@hotmail.com>
2023-05-16 19:52:49 -0300

    Unit test

--- src/services/register.spec.ts
+++ src/services/register.spec.ts
@@ -1,5 +1,34 @@
-import { expect, test } from 'vitest'
+import { expect, describe, it } from 'vitest'
+import { RegisterUseCase } from './register'
+import { compare } from 'bcryptjs'
 
-test('check if it works', () => {
-  expect(2 + 2).toBe(4)
+describe('Register Use Case', () => {
+  it('should hash user password upon registration', async () => {
+    const registerUseCase = new RegisterUseCase({
+      async findByEmail(email) {
+        return null
+      },
+      async create(data) {
+        return {
+          id: 'user-1',
+          name: data.email,
+          email: data.email,
+          password_hash: data.password_hash,
+          created_at: new Date(),
+        }
+      },
+    })
+    const userPassowrd = '123456'
+
+    const { user } = await registerUseCase.execute({
+      name: 'John Doe',
+      email: 'johndoe@example.com',
+      password: userPassowrd,
+    })
+    const isPasswordCorrectlyHashed = await compare(
+      userPassowrd,
+      user.password_hash,
+    )
+    expect(isPasswordCorrectlyHashed).toBe(true)
+  })
 })
